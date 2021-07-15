1b792a478e2d190fbd4666e9a4407eaad2b4e91b
38takumi <saitout485@gmail.com>
2021-07-15 08:21:38 +0000

    3章”
    git commit -m 3章”

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '47aa5c4aafda1f97c3372c143f13831d15e9e98acd9e06ec301ae2a6ae385867076dbd049c4ca0c75673a9094780eaf88933bfb78dd58f6638da8394e74e0760'
+
