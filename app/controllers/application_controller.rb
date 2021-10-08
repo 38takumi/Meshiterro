b62570df5e6585b39f182c40ab322c65b150211f
38takumi <saitout485@gmail.com>
2021-10-08 15:16:34 +0000

    [fix]インデント・コメント修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,7 @@ class ApplicationController < ActionController::Base
     post_images_path
   end
 
-
-
-
   protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,22 +5,15 @@ class PostCommentsController < ApplicationController
     comment.post_image_id = post_image.id
     comment.save
     redirect_to post_image_path(post_image)
-    
   end
 
   def destroy
     PostComment.find_by(id: params[:id], post_image_id: params[:post_image_id]).destroy
     redirect_to post_image_path(params[:post_image_id])
   end
-  
-  
-  
-  private
 
+  private
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
-
-  
-  
 end
